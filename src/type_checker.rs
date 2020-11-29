a6b7e653d136cb11b86b875bcc1f6ee9815a016e
Adam Czajkowski <a.c.czajkowski@gmail.com>
2020-11-29 18:16:24 +0100

    match => let

--- src/type_checker.rs
+++ src/type_checker.rs
@@ -31,64 +31,40 @@ pub fn check_stmt(stmt: &ast::Node<ast::Stmt>, mut venv: &mut VEnv, fenv: &FEnv,
 pub fn check_fn(fdef: &ast::Node<ast::FunDef>, fenv: &FEnv, source: &str) -> Result<(), String> {
     let mut venv = HashMap::new();
 
-    match fdef.node() {
-        (_, ident_node, arg_nodes, block_node) => {
-            for arg_node in arg_nodes {
-                match arg_node.node() {
-                    (arg_prim_node, arg_ident_node) => {
-                        match (arg_prim_node.node(), arg_ident_node.node()) {
-                            (prim, ident) => {
-                                match venv.insert(ident.clone(), prim.clone()) {
-                                    None => (),
-                                    Some(_) => {
-                                        let msg = format!("Argument name {} repeated in function {}",
-                                            ident, source_token(source, ident_node.span()));
-                                        return Err(msg)
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-
-            match block_node.node() {
-                stmts =>  {
-                    for stmt in stmts {
-                        check_stmt(&stmt, &mut venv, fenv, source)?;
-                    }
-                    Ok(())
-                },
-            }
+    let (_, ident_node, arg_nodes, block_node) = fdef.node();
+    for arg_node in arg_nodes {
+        let (arg_prim_node, arg_ident_node) = arg_node.node();
+        let (prim, ident) = (arg_prim_node.node(), arg_ident_node.node());
+        if let Some(_) = venv.insert(ident.clone(), prim.clone()) {
+            let msg = format!("Argument name {} repeated in function {}",
+                ident, source_token(source, ident_node.span()));
+            return Err(msg)
         }
     }
+
+    let stmts = block_node.node();
+    for stmt in stmts {
+        check_stmt(&stmt, &mut venv, fenv, source)?;
+    }
+    Ok(())
 }
 
 pub fn fn_env(fdefs: &Vec<ast::Node<ast::FunDef>>, source: &str) -> Result<FEnv, String> {
     let mut fenv = HashMap::new();
 
     for fdef in fdefs.iter() {
-        match fdef.node() {
-            (prim_node, ident_node, arg_nodes, _) => {
-                let arg_types = arg_nodes.iter().map(|arg_node| {
-                    match arg_node.node() {
-                        (prim_node, _) => prim_node.node().clone()
-                    }
-                }).collect();
-
-                match (prim_node.node(), ident_node.node()) {
-                    (fn_type, fn_name) => {
-                        match fenv.insert(fn_name.clone(), (fn_type.clone(), arg_types)) {
-                            None => (),
-                            Some(_) => {
-                                let msg = format!("Function name {} not unique", fn_name);
-                                return Err(msg)
-                            }
-                        }
-                    }
-                }
+        let (prim_node, ident_node, arg_nodes, _) = fdef.node();
+        let arg_types = arg_nodes.iter().map(|arg_node| {
+            match arg_node.node() {
+                (prim_node, _) => prim_node.node().clone()
             }
-        };
+        }).collect();
+
+        let (fn_type, fn_name) = (prim_node.node(), ident_node.node());
+        if let Some(_) = fenv.insert(fn_name.clone(), (fn_type.clone(), arg_types)) {
+            let msg = format!("Function name {} not unique", fn_name);
+            return Err(msg)
+        }
     }
 
     Ok(fenv)
