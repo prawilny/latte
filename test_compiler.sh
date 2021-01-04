c8eab95d19185ddfac380b09d17069c05cea7e3d
Adam Czajkowski <a.c.czajkowski@gmail.com>
2021-01-04 18:36:25 +0100

    test compiler

---
+++ test_compiler.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+BASEDIR=$(dirname "$0")
+COMPILER=$BASEDIR/latc_x86_64
+BLOB=$1
+
+for FILE_PATH in $BLOB # good/*.lat
+do
+    FILE_PATH_STEM=${FILE_PATH%.lat}
+    $COMPILER $FILE_PATH_STEM.lat
+
+    $FILE_PATH_STEM > $FILE_PATH_STEM.result
+
+    diff $FILE_PATH_STEM.{output,result}
+done
