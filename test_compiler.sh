7aab80fca269c5f05d7f44ae9104b3bacd03b61e
Adam Czajkowski <a.c.czajkowski@gmail.com>
2021-01-05 19:04:13 +0100

    test_compiler: input

--- test_compiler.sh
+++ test_compiler.sh
@@ -9,9 +9,15 @@ BLOB=$1
 for FILE_PATH in $BLOB # good/*.lat
 do
     FILE_PATH_STEM=${FILE_PATH%.lat}
+    if [ -f "$" ]; then
+        INPUT_FILE=$FILE_PATH_STEM.input
+    else
+        INPUT_FILE="/dev/null"
+    fi
+
     $COMPILER $FILE_PATH_STEM.lat
 
-    $BASEDIR/$FILE_PATH_STEM > $FILE_PATH_STEM.result
+    $BASEDIR/$FILE_PATH_STEM < $INPUT_FILE > $FILE_PATH_STEM.result
 
     echo diff $FILE_PATH_STEM
     diff $FILE_PATH_STEM.{output,result}
