181d3222fdb4c62f6e92e6bfbd2aa43fd9da1bb5
Adam Czajkowski <a.c.czajkowski@gmail.com>
2021-01-06 19:50:49 +0100

    test_compiler -= loop

--- test_compiler.sh
+++ test_compiler.sh
@@ -4,19 +4,16 @@ set -e
 
 BASEDIR=$(dirname "$0")
 COMPILER=$BASEDIR/latc_x86_64
-BLOB=$1
+FILE_PATH=$1
 
-for FILE_PATH in $BLOB # good/*.lat
-do
-    FILE_PATH_STEM=${FILE_PATH%.lat}
-    INPUT_FILE=$FILE_PATH_STEM.input
+FILE_PATH_STEM=${FILE_PATH%.lat}
+INPUT_FILE=$FILE_PATH_STEM.input
 
-    echo $FILE_PATH_STEM:
+echo $FILE_PATH_STEM:
 
-    $COMPILER $FILE_PATH_STEM.lat
+$COMPILER $FILE_PATH_STEM.lat
 
-    touch $INPUT_FILE
-    cat $INPUT_FILE | $BASEDIR/$FILE_PATH_STEM 1>$FILE_PATH_STEM.result
+touch $INPUT_FILE
+cat $INPUT_FILE | $BASEDIR/$FILE_PATH_STEM 1>$FILE_PATH_STEM.result
 
-    diff $FILE_PATH_STEM.{output,result}
-done
+diff $FILE_PATH_STEM.{output,result}
