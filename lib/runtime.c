2c4a933ad21a6a9fd7b939ac4e938805cc504b88
Adam Czajkowski <a.c.czajkowski@gmail.com>
2021-01-23 13:18:16 +0100

    runtime: remove __drop

--- lib/runtime.c
+++ lib/runtime.c
@@ -60,29 +60,3 @@ void * __new(const i64 bytes, const void * vtable) {
     *((i64 *) ptr) = (i64) vtable;
     return ptr;
 }
-
-void __drop(void * ptr) {
-    if (ptr == NULL) {
-        return;
-    }
-    i64 * counter = (i64 *) ptr;
-    i64 * bitset = (i64 *) ptr + 8;
-    // void * vtable = ptr + 16;
-    void ** fields = (void **) ptr + 24;
-
-    switch(*counter) {
-        case -1: // const string
-            return;
-        case 1:
-            for (size_t i = 0; i < 64; i++) {
-                if (*bitset & (1u << i)) {
-                    __drop(*(fields + i * sizeof(i64))); // dobry offset?
-                }
-            }
-            free(ptr);
-            return;
-        default:
-            *counter -= 1;
-            return;
-    }
-}
